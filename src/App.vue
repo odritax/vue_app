ddec721d005d6148f8059c2b3af230a1298ea834
Odritax <pabloyanezbarriga@gmail.com>
2020-07-10 14:01:08 -0400

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Lista msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+<script>
+import Lista from './components/Lista.vue'
+export default {
+  name: 'App',
+  components: {
+    Lista
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Lista.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Las cosas que quiere comer {{ nombre }}: </h1>
+    <ul>
+      <li v-for="(lista,i) in listas" v-bind:key="i"> {{lista}}</li>
+     </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Lista',
+  data:function(){
+    return{
+      nombre:'Odrita',
+      listas:['Pizza','Sushi','Cazuela']                                        
+    }
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 15px 10px;
+  align-text:justify;
+}
+</style>
